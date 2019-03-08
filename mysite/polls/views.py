8ebd384135ec9eecbba0f9c40db90f4d7c8e7061
Mike <mike@arsquanta.com>
2019-03-08 07:57:27 -0800

    using templates and render()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
 from .models import Question
 
 # last 5 questions
+# using render does not require loader import
+# since it knows how to work with templates
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
